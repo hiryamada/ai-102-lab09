5fc91ef60be6de49babbce4a25571c644e75377b
hiryamada <71864722+hiryamada@users.noreply.github.com>
2023-06-26 14:47:25 +0900

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install -y dotnet-7.0-sdk
 tzutil /s "Tokyo Standard Time"
-dotnet new worker -n lab9 -o ~/Desktop/lab9
-code ~/Desktop/lab9
+git clone https://github.com/hiryamada/ai-102-lab9.git ~/Documents/lab9
+code ~/Documents/lab9
